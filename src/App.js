f09d2ba9fcacb4ef1756f7efed544b182039b3df
Francisco Diaz <franciscojavier.diaz@i3television.es>
2018-02-05 10:07:54 +0100

    Change App to stateless component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import React from 'react';
 
-class App extends React.Component {
-    render() {
-        return (<h1>Hello</h1>)
-    }
-}
+const App = () => (<h1>Hello</h1>);
 
 export default App;
